c8c50d1320fe42fa8804042178903e6375e8b35a
Mike McGrath <mike@mikemcgrathonline.us>
2022-02-07 21:50:32 -0500

    setup

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: [
+    "../src/components/stories/*.stories.mdx",
+    "../src/components/stories/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
