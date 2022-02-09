df59d029773b1537d5fcb58920aef97ceffb7492
Mike McGrath <mike@mikemcgrathonline.us>
2022-02-09 00:20:42 -0500

    Works

--- src/components/stories/Stepper.stories.js
+++ src/components/stories/Stepper.stories.js
@@ -27,67 +27,70 @@ const Button = styled.button`
   padding: 6px 12px;
 `;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const ExampleStepper = () => {
-  const { incrementCurrentStep, decrementCurrentStep, setStepCompleted } =
-    useStepper();
+export const BasicUsageExamaple = () => {
+  const ExampleStepper = () => {
+    const { incrementCurrentStep, decrementCurrentStep, setStepCompleted } =
+      useStepper();
+    return (
+      <Stepper>
+        <Stepper.Steps>
+          <Stepper.Step id="first" name="Business Info">
+            <StepBody>
+              <p>This is step 1</p>
+              <div>
+                <Button onClick={incrementCurrentStep}>Next step</Button>
+                <Button onClick={() => setStepCompleted("first")}>
+                  Complete Step
+                </Button>
+              </div>
+            </StepBody>
+          </Stepper.Step>
+          <Stepper.Step id="second" name="Branding">
+            <StepBody>
+              <p>this is step 2</p>
+              <div>
+                <Button onClick={decrementCurrentStep}>Previous step</Button>
+                <Button onClick={incrementCurrentStep}>Next step</Button>
+                <Button onClick={() => setStepCompleted("second")}>
+                  Complete Step
+                </Button>
+              </div>
+            </StepBody>
+          </Stepper.Step>
+          <Stepper.Step id="third" name="Goal">
+            <StepBody>
+              <p>this is step 3</p>
+              <div>
+                <Button onClick={decrementCurrentStep}>Previous step</Button>
+                <Button onClick={incrementCurrentStep}>Next step</Button>
+                <Button onClick={() => setStepCompleted("third")}>
+                  Complete Step
+                </Button>
+              </div>
+            </StepBody>
+          </Stepper.Step>
+          <Stepper.Step id="forth" name="Preview">
+            <StepBody>
+              <p>this is step 4</p>
+              <div>
+                <Button onClick={decrementCurrentStep}>Previous step</Button>
+                <Button onClick={incrementCurrentStep}>Next step</Button>
+                <Button onClick={() => setStepCompleted("forth")}>
+                  Submit
+                </Button>
+              </div>
+            </StepBody>
+          </Stepper.Step>
+        </Stepper.Steps>
+      </Stepper>
+    );
+  };
+
   return (
-    <Stepper>
-      <Stepper.Steps>
-        <Stepper.Step id="first" name="Business Info">
-          <StepBody>
-            <p>This is step 1</p>
-            <div>
-              <Button onClick={incrementCurrentStep}>Next step</Button>
-              <Button onClick={() => setStepCompleted("first")}>
-                Complete Step
-              </Button>
-            </div>
-          </StepBody>
-        </Stepper.Step>
-        <Stepper.Step id="second" name="Branding">
-          <StepBody>
-            <p>this is step 2</p>
-            <div>
-              <Button onClick={decrementCurrentStep}>Previous step</Button>
-              <Button onClick={incrementCurrentStep}>Next step</Button>
-              <Button onClick={() => setStepCompleted("second")}>
-                Complete Step
-              </Button>
-            </div>
-          </StepBody>
-        </Stepper.Step>
-        <Stepper.Step id="third" name="Goal">
-          <StepBody>
-            <p>this is step 3</p>
-            <div>
-              <Button onClick={decrementCurrentStep}>Previous step</Button>
-              <Button onClick={incrementCurrentStep}>Next step</Button>
-              <Button onClick={() => setStepCompleted("third")}>
-                Complete Step
-              </Button>
-            </div>
-          </StepBody>
-        </Stepper.Step>
-        <Stepper.Step id="forth" name="Preview">
-          <StepBody>
-            <p>this is step 4</p>
-            <div>
-              <Button onClick={decrementCurrentStep}>Previous step</Button>
-              <Button onClick={incrementCurrentStep}>Next step</Button>
-              <Button onClick={() => setStepCompleted("forth")}>Submit</Button>
-            </div>
-          </StepBody>
-        </Stepper.Step>
-      </Stepper.Steps>
-    </Stepper>
+    <StepperProvider>
+      <ExampleStepper />
+    </StepperProvider>
   );
 };
-
-export const Primary = () => (
-  <StepperProvider>
-    <ExampleStepper />
-  </StepperProvider>
-);
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {};
+BasicUsageExamaple.args = {};
